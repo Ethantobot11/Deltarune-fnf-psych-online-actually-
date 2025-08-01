61d79213d1f3200a43b665e1c5c30725400c898a
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-08-02 01:17:23 +0200

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -35,6 +35,19 @@ import backend.Difficulty;
 import backend.Mods;
 import mobile.backend.StorageUtil;
 
+//Android
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
 import objects.Alphabet;
 import objects.BGSprite;
 
