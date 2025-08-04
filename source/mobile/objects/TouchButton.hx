9ba50effb3d3bb7248817e5dc5f0f8a2a1bf2931
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-08-04 02:11:36 +0200

    Update TouchButton.hx

--- source/mobile/objects/TouchButton.hx
+++ source/mobile/objects/TouchButton.hx
@@ -9,7 +9,6 @@ import flixel.util.FlxDestroyUtil.IFlxDestroyable;
 import flixel.input.mouse.FlxMouseButton;
 #end
 import shaders.flixel.system.FlxShader;
-import flixel.utils.FlxDestroyUtil;
 
 
 /**
