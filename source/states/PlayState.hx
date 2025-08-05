4dea2dc2ae3c43d84c7cf1ff8feab702d13d34d3
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-08-05 23:16:40 +0200

    Add files via upload

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -500,8 +500,6 @@ class PlayState extends MusicBeatState
 
 	var noteUnderlays:FlxTypedGroup<FlxSprite>;
 
-	public var luaTouchPad:TouchPad;
-
 	public var playOtherSide:Bool = false;
 
 	var nameplates:FlxTypedGroup<FlxText> = new FlxTypedGroup<FlxText>();
@@ -1255,7 +1253,7 @@ class PlayState extends MusicBeatState
 
 		if (GameClient.isConnected()) {
 			preloadTasks.push(() -> {
-				waitReadySpr = new Alphabet(0, 0, controls.mobileC ? "TOUCH YOUR SCREEN TO START" : "PRESS ACCEPT TO START", true);
+				waitReadySpr = new Alphabet(0, 0, "PRESS ACCEPT TO START", true);
 				waitReadySpr.cameras = [camOther];
 				waitReadySpr.alignment = CENTERED;
 				waitReadySpr.x = FlxG.width / 2;
@@ -1928,27 +1926,7 @@ class PlayState extends MusicBeatState
 			setOnScripts('defaultOpponentStrumY' + i, opponentStrums.members[i].y);
 			// if(ClientPrefs.data.middleScroll) opponentStrums.members[i].visible = false;
 		}
-		
-		addHitbox();
-		addTouchPad((replayData != null || cpuControlled) ? 'LEFT_RIGHT' : 'NONE', (GameClient.isConnected()) ? 'P_C_T' : (replayData != null || cpuControlled) ? #if android 'X_Y' : 'T' #else 'P_X_Y' : 'P_T' #end);
-		addTouchPadCamera();
-		hitbox.onButtonDown.add(onButtonPress);
-		hitbox.onButtonUp.add(onButtonRelease);
-		if (replayData == null && !cpuControlled)
-			hitbox.visible = true;
-		hitbox.forEachAlive((button) ->
-		{
-			if (touchPad.buttonT != null)
-    				button.deadZones.push(touchPad.buttonT);
-			if (touchPad.buttonC != null)
-    				button.deadZones.push(touchPad.buttonC);
-			if (touchPad.buttonP != null)			
-					button.deadZones.push(touchPad.buttonP);
-		});
-		if (replayRecorder != null)
-			replayRecorder.setupMobileCRecorder();
 	}
-	
 
 	public function startCountdown()
 	{
@@ -2867,7 +2845,7 @@ class PlayState extends MusicBeatState
 			// }
 
 			if (cpuControlled) {
-				var shiftMult = (touchPad.buttonX.pressed || FlxG.keys.pressed.SHIFT) ? 3 : 1;
+				var shiftMult = FlxG.keys.pressed.SHIFT ? 3 : 1;
 				if (controls.UI_LEFT) {
 					if (playbackRate - elapsed * 0.25 * shiftMult > 0)
 						playbackRate -= elapsed * 0.25 * shiftMult;
@@ -2883,13 +2861,12 @@ class PlayState extends MusicBeatState
 					}
 					botplayTxt.text = "BOTPLAY\n" + '(${CoolUtil.floorDecimal(playbackRate, 2)}x)';
 				}
-				else if (touchPad.buttonY.justPressed || controls.RESET) {
+				else if (controls.RESET) {
 					playbackRate = 1;
-					botplayTxt.text = "BOTPLAY";
 				}
 			}
 		}
-		
+
 		if (FlxG.keys.justPressed.F11 && GameClient.isConnected()) {
 			GameClient.reconnect(5); //delay the reconnection for 5 seconds (for testing)
 		}
@@ -2908,7 +2885,7 @@ class PlayState extends MusicBeatState
 			// 	endSong();
 			// }
 
-			if (canStart && !isReady && (controls.mobileC && FlxG.mouse.justPressed || controls.ACCEPT) && canInput()) {
+			if (!isReady && controls.ACCEPT && !inCutscene && canStart && canInput()) {
 				isReady = true;
 				FlxG.sound.play(Paths.sound('confirmMenu'), 0.5);
 				if (ClientPrefs.data.flashing)
@@ -3294,7 +3271,7 @@ class PlayState extends MusicBeatState
 
 	function pause() {
 		FlxG.camera.followLerp = 0;
-		touchPad.visible = persistentUpdate = false;
+		persistentUpdate = false;
 		persistentDraw = true;
 		paused = true;
 
@@ -3308,8 +3285,6 @@ class PlayState extends MusicBeatState
 	function resume() {
 		if (forcePause)
 			return;
-
-		touchPad.visible = true;
 
 		if (FlxG.sound.music != null && !startingSong) {
 			resyncVocals();
@@ -3946,7 +3921,6 @@ class PlayState extends MusicBeatState
 	public var transitioning = false;
 	public function endSong()
 	{
-		hitbox.visible = #if !android touchPad.visible = #end false;
 		if (redditMod) {
 			health = 0;
 			doDeathCheck();
@@ -4818,28 +4792,6 @@ class PlayState extends MusicBeatState
 			}
 		}
 		return -1;
-	}
-
-        private function onButtonPress(button:TouchButton, ids:Array<MobileInputID>):Void
-	{
-		if (ids.filter(id -> id.toString().startsWith("EXTRA")).length > 0 || ids.filter(id -> id.toString().startsWith("TAUNT")).length > 0)
-			return;
-
-		var buttonCode:Int = (ids[0].toString().startsWith('NOTE')) ? ids[0] : ids[1];
-		callOnScripts('onButtonPressPre', [buttonCode]);
-		if (button.justPressed) keyPressed(buttonCode);
-		callOnScripts('onButtonPress', [buttonCode]);
-	}
-
-	private function onButtonRelease(button:TouchButton, ids:Array<MobileInputID>):Void
-	{
-		if (ids.filter(id -> id.toString().startsWith("EXTRA")).length > 0 || ids.filter(id -> id.toString().startsWith("TAUNT")).length > 0)
-			return;
-
-		var buttonCode:Int = (ids[0].toString().startsWith('NOTE')) ? ids[0] : ids[1];
-		callOnScripts('onButtonReleasePre', [buttonCode]);
-		if(buttonCode > -1) keyReleased(buttonCode);
-		callOnScripts('onButtonRelease', [buttonCode]);
 	}
 
 	// Hold notes
@@ -6369,100 +6321,5 @@ class PlayStatePlayer {
 
 	function new(player:Player) {
 		this.player = player;
-	}
-
-	public function makeLuaTouchPad(DPadMode:String, ActionMode:String) {
-		if(members.contains(luaTouchPad)) return;
-
-		if(!variables.exists("luaTouchPad"))
-			variables.set("luaTouchPad", luaTouchPad);
-
-		luaTouchPad = new TouchPad(DPadMode, ActionMode);
-		luaTouchPad.alpha = ClientPrefs.data.controlsAlpha;
-	}
-	
-	public function addLuaTouchPad() {
-		if(luaTouchPad == null || members.contains(luaTouchPad)) return;
-
-		var target = LuaUtils.getTargetInstance();
-		target.insert(target.members.length + 1, luaTouchPad);
-	}
-
-	public function addLuaTouchPadCamera() {
-		if(luaTouchPad != null)
-			luaTouchPad.cameras = [luaTpadCam];
-	}
-
-	public function removeLuaTouchPad() {
-		if (luaTouchPad != null) {
-			luaTouchPad.kill();
-			luaTouchPad.destroy();
-			remove(luaTouchPad);
-			luaTouchPad = null;
-		}
-	}
-
-	public function luaTouchPadPressed(button:Dynamic):Bool {
-		if(luaTouchPad != null) {
-			if(Std.isOfType(button, String))
-				return luaTouchPad.buttonPressed(MobileInputID.fromString(button));
-			else if(Std.isOfType(button, Array)){
-				var FUCK:Array<String> = button; // haxe said "You Can't Iterate On A Dyanmic Value Please Specificy Iterator or Iterable *insert nerd emoji*" so that's the only i foud to fix
-				var idArray:Array<MobileInputID> = [];
-				for(strId in FUCK)
-					idArray.push(MobileInputID.fromString(strId));
-				return luaTouchPad.anyPressed(idArray);
-			} else
-				return false;
-		}
-		return false;
-	}
-
-	public function luaTouchPadJustPressed(button:Dynamic):Bool {
-		if(luaTouchPad != null) {
-			if(Std.isOfType(button, String))
-				return luaTouchPad.buttonJustPressed(MobileInputID.fromString(button));
-			else if(Std.isOfType(button, Array)){
-				var FUCK:Array<String> = button;
-				var idArray:Array<MobileInputID> = [];
-				for(strId in FUCK)
-					idArray.push(MobileInputID.fromString(strId));
-				return luaTouchPad.anyJustPressed(idArray);
-			} else
-				return false;
-		}
-		return false;
-	}
-	
-	public function luaTouchPadJustReleased(button:Dynamic):Bool {
-		if(luaTouchPad != null) {
-			if(Std.isOfType(button, String))
-				return luaTouchPad.buttonJustReleased(MobileInputID.fromString(button));
-			else if(Std.isOfType(button, Array)){
-				var FUCK:Array<String> = button;
-				var idArray:Array<MobileInputID> = [];
-				for(strId in FUCK)
-					idArray.push(MobileInputID.fromString(strId));
-				return luaTouchPad.anyJustReleased(idArray);
-			} else
-				return false;
-		}
-		return false;
-	}
-
-	public function luaTouchPadReleased(button:Dynamic):Bool {
-		if(luaTouchPad != null) {
-			if(Std.isOfType(button, String))
-				return luaTouchPad.buttonReleased(MobileInputID.fromString(button));
-			else if(Std.isOfType(button, Array)){
-				var FUCK:Array<String> = button;
-				var idArray:Array<MobileInputID> = [];
-				for(strId in FUCK)
-					idArray.push(MobileInputID.fromString(strId));
-				return luaTouchPad.anyReleased(idArray);
-			} else
-				return false;
-		}
-		return false;
 	}
 }
